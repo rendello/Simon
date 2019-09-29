b93626d8d1898a21b51bfcce713e927594ca5a5d
Gaven Rendell <gavenrendell@protonmail.com>
2019-09-28 20:59:31 -0400

    Simon? I meant to type Simón!

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from discord.ext import commands
 
 from secrets import discord_secret
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix="!")
+
 
 @bot.command()
 async def simon(ctx):
-    string = "hi"
+    string = "Welcome to *Simón!*"
+    await ctx.add_reaction('\:green_heart:')
     await ctx.send(string)
 
 bot.run(discord_secret)
