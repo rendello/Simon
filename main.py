6935580061e7e99375efcfda5596f32b6fe08696
Gaven Rendell <gavenrendell@protonmail.com>
2019-09-29 18:37:04 -0400

    now sees reacts and react removals on its posts

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from secrets import discord_secret
 bot = commands.Bot(command_prefix="!")
 
 
+async def button_pressed(*, user, button):
+    print(user, button)
+
+
 @bot.command()
 async def simon(ctx):
     string = "Welcome to *Simón!*"
@@ -20,6 +24,16 @@ async def on_reaction_add(reaction, user):
     message = reaction.message
 
     if message.author.id == bot.user.id:
+        await button_pressed(user=user.id, button=reaction)
+        await message.channel.send(reaction)
+
+
+@bot.event
+async def on_reaction_remove(reaction, user):
+    message = reaction.message
+
+    if message.author.id == bot.user.id:
+        await button_pressed(user=user.id, button=reaction)
         await message.channel.send(reaction)
 
 
